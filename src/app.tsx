38d7bd925fb5ecacc0f39ece9156c45726827b7e
sk-1982 <13491412+sk-1982@users.noreply.github.com>
2024-05-14 05:22:51 -0400

    dynamic program loading

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import { Taskbar } from './components/taskbar.tsx';
 import { Providers } from './providers.tsx';
-import { CalculatorProgram } from './programs/calculator.tsx';
+import { DynamicProgram } from './components/program.tsx';
 
 export function App() {
-
   return (<Providers>
-    <CalculatorProgram />
+    <DynamicProgram name="calc.exe" load={async () => {
+      const { CalculatorProgram } = (await import('./programs/calculator.tsx'));
+      return (<CalculatorProgram />);
+    }} />
     <Taskbar />
   </Providers>);
 }
--- src/components/program.tsx
+++ src/components/program.tsx
@@ -1,5 +1,5 @@
-import { ComponentChildren, createContext } from 'preact';
-import { useCallback, useContext, useEffect, useRef, useState } from 'preact/hooks';
+import { ComponentChildren, createContext, VNode } from 'preact';
+import { useCallback, useContext, useEffect, useLayoutEffect, useRef, useState } from 'preact/hooks';
 import { ContextWindow, useWindows, Window, WindowProps } from './window.tsx';
 
 type Program = {
@@ -11,23 +11,32 @@ const ProgramContext = createContext<{
 	register: (program: Program) => void,
 	unregister: (name: string) => void,
 	openProgram: (name: string, ...args: string[]) => void,
-}>({ register: () => {}, unregister: () => {}, openProgram: () => {} });
+	queueOpen: (name: string, ...args: string[]) => void,
+}>({ register: () => {}, unregister: () => {}, openProgram: () => {}, queueOpen: () => {} });
 
 export const usePrograms = () => useContext(ProgramContext);
 
 export const ProgramContextProvider = ({ children }: { children: ComponentChildren }) => {
 	const [programList, setProgramList] = useState<Program[]>([]);
+	const queuedLaunches = useRef<Record<string, string[]>>({});
 	const openProgram = useCallback((name: string, ...args: string[]) => {
 		programList.find(p => p.name === name)?.onOpen(...args);
 	}, [programList]);
 	const register = useCallback((program: Program) => {
 		setProgramList(l => [...l, program]);
+		if (queuedLaunches.current[program.name] !== undefined) {
+			program.onOpen(...queuedLaunches.current[program.name]);
+			delete queuedLaunches.current[program.name];
+		}
 	}, [setProgramList]);
 	const unregister = useCallback((name: string) => {
 		setProgramList(l => l.filter(p => p.name !== name));
 	}, [setProgramList]);
+	const queueOpen = useCallback((name: string, ...args: string[]) => {
+		queuedLaunches.current[name] = args;
+	}, []);
 
-	return (<ProgramContext.Provider value={{ openProgram, unregister, register }}>
+	return (<ProgramContext.Provider value={{ openProgram, unregister, register, queueOpen }}>
 		{ children }
 	</ProgramContext.Provider>)
 };
@@ -35,11 +44,11 @@ export const ProgramContextProvider = ({ children }: { children: ComponentChildr
 type ProgramProps = {
 	name: string,
 	onOpen: (...args: string[]) => void,
-	children: ComponentChildren
+	children?: ComponentChildren
 };
 
 export const Program = ({ name, onOpen, children }: ProgramProps) => {
-	const context = useContext(ProgramContext);
+	const context = usePrograms();
 
 	useEffect(() => {
 		context.register({ name, onOpen });
@@ -66,4 +75,33 @@ export const SimpleProgram = ({ name, children, ...windowProps }: SimpleProgramP
 			{ children }
 		</Window>
 	</Program>);
+};
+
+export const DynamicProgram = ({ name, load }: { name: string, load: () => Promise<VNode<any>> }) => {
+	const loading = useRef(false);
+	const [loaded, setLoaded] = useState<VNode<any> | null>(null);
+	const programs = usePrograms();
+
+	useEffect(() => {
+		programs.register({ name, onOpen: (...args) => {
+			if (loading.current) return;
+			document.body.dataset.loading = (+(document.body.dataset.loading ?? 0) + 1).toString();
+			loading.current = true;
+			programs.unregister(name);
+			programs.queueOpen(name, ...args);
+			load().then(res => {
+				setLoaded(res);
+				const val = +(document.body.dataset.loading ?? 1) - 1;
+				if (val)
+					document.body.dataset.loading = val.toString();
+				else
+					delete document.body.dataset.loading;
+			})
+		} })
+	}, []);
+
+	if (loaded)
+		return loaded;
+
+	return null;
 };
